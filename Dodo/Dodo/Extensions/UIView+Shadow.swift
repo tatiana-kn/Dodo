42b78ae9ad7330634d1af71877083e69f4541eee
tatiana-kn <123388284+tatiana-kn@users.noreply.github.com>
2024-06-29 13:44:56 +0400

    created MenuScreen

---
+++ Dodo/Dodo/Extensions/UIView+Shadow.swift
@@ -0,0 +1,8 @@
+//
+//  UIView+Shadow.swift
+//  Dodo
+//
+//  Created by Tia M on 6/29/24.
+//
+
+import Foundation
---
+++ Dodo/Dodo/Screens/Menu/Cells/ProductCell.swift
@@ -0,0 +1,8 @@
+//
+//  ProductCell.swift
+//  Dodo
+//
+//  Created by Tia M on 6/29/24.
+//
+
+import Foundation
